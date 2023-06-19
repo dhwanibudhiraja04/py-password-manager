173f1d3a83b68952b07a37c71a36cc41a9713a0a
Dhwani <dhwanibudhiraja2004@gmail.com>
2023-06-19 09:44:08 +0530

    first modification

--- storage.py
+++ storage.py
@@ -1,3 +1,3 @@
-accounts = [['dhwani', 'dhwani']]
-masterpassword = [['dhwani', 'netflix', 'dhwani', 'dhwani']]
+accounts = [['dhwani', 'dhwani'], ['omkarkadle', 'Omkar1505']]
+masterpassword = [['dhwani', 'netflix', 'dhwani', 'dhwani'], ['dhwani', 'fb', 'sahil', 'sahil'], ['omkarkadle', 'netflix.in', 'netflixandchill', 'OmkarKadle2003']]
 masterpassword = [['dhwani', 'netflix', 'dhwani', 'dhwani'], ['dhwani', 'fb', 'sahil', 'sahil']]
